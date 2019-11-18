997e84fa3c24c429bd1e76e3b26aa2e6c6ac2927
yanaazaryan <57559475+yanaazaryan@users.noreply.github.com>
2019-11-18 13:38:21 +0400

    fixed bug

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const number = reader.questionInt("Enter the number that you want to convert = "
 const variant = reader.questionInt("Enter variant: \n 1:Binary \n 2:Hexadecimal \n");
 
 if(variant === 1){
-  console.log(convertToBinary(number));
+  console.log(utils.convertToBinary(number));
 }else if(variant === 2){
-  console.log(convertToHexa(number));
+  console.log(utils.convertToHexa(number));
 }else if(typeof variant === "number"){
   console.log("No variants for that number ");
 }else{
